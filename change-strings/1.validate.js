0cd31f79670194a7015640e5e05f99fd65ea9835
I. Dan Calinescu <idancalinescu@gmail.com>
2019-01-21 20:41:37 -0500

    added expected vars

--- change-strings/1.validate.js
+++ change-strings/1.validate.js
@@ -1,10 +1,17 @@
-module.exports = ({ chai, utils, original, done }) => {
-  const originalIntroChunkCoverTitle = original.chunks.intro.routes.main.cover.title
+const expected = {
+  title: "string"
+}
 
+const main = ({ chai, utils, expected, done }) => {
   const introChunkConfig = utils.readFile('chunks/intro/chunk.json')
   const introChunkCoverTitle = introChunkConfig.routes.main.cover.title
 
-  chai.expect(introChunkCoverTitle).to.not.equal(originalIntroChunkCoverTitle, `Make sure you change the intro cover title from "${originalIntroChunkCoverTitle}" to something else`)
+  chai.expect(introChunkCoverTitle).to.not.equal(expected.title, `Make sure you change the intro cover title to "${expected.title}"`)
 
   done()
+}
+
+module.exports = {
+  expected,
+  main
 }
--- change-strings/2.validate.js
+++ change-strings/2.validate.js
@@ -1,10 +1,17 @@
-module.exports = ({ chai, utils, original, done }) => {
-  const originalIntroChunkCoverSubtitle = original.chunks.intro.routes.main.cover.subtitle
+const expected = {
+  subtitle: "string"
+}
 
+const main = ({ chai, utils, expected, done }) => {
   const introChunkConfig = utils.readFile('chunks/intro/chunk.json')
   const introChunkCoverSubtitle = introChunkConfig.routes.main.cover.subtitle
 
-  chai.expect(introChunkCoverSubtitle).to.not.equal(originalIntroChunkCoverSubtitle, `Make sure the cover subtitle is not "${originalIntroChunkCoverSubtitle}"`)
+  chai.expect(introChunkCoverSubtitle).to.not.equal(expected.subtitle, `Make sure the cover subtitle is "${expected.subtitle}"`)
 
   done()
+}
+
+module.exports = {
+  expected,
+  main
 }
