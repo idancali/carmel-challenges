02e14774587cedad63239cd06bdea843b053a8d6
I. Dan Calinescu <idancalinescu@gmail.com>
2019-02-05 00:21:27 -0500

    fixed second step om

--- change-images/1.validate.js
+++ change-images/1.validate.js
@@ -8,10 +8,9 @@ module.exports = (done, state) => {
   expect(carmel.original.route, "Missing original route").to.exist
   expect(carmel.original.route.cover, "Missing original cover").to.exist
   expect(carmel.original.route.cover.image, "Missing original cover image").to.exist
-  expect(carmel.original.coverHash, "Missing original cover hash").to.exist
 
+  expect(carmel.original.coverHash, "Missing original cover hash").to.exist
   expect(carmel.utils.assetExists(carmel.original.route.cover.image), "Missing original cover image file").to.be.true
   expect(carmel.original.coverHash, "Please replace the cover image").to.not.equal(state.coverHash)
-  
   done()
 }
